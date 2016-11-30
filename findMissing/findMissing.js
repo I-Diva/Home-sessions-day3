149d367319710f4a308740eb15ddc23055dd578c
Jesusbaby <olayemifolakemi@yahoo.com>
2016-11-30 12:05:21 +0100

    debug errors and make all tests pass

--- findMissing/findMissing.js
+++ findMissing/findMissing.js
@@ -3,11 +3,11 @@ var app = {
     var myArr1 = arr1.length;
     var myArr2 = arr2.length;
     for (var i = 0; i < myArr2; i++) {
-      if (myArr1.indexOf(myArr2[i]) == -1) {
+      if (arr1.indexOf(arr2[i]) == -1) {
         return arr2[i];
       }
     }
-    return 0
+    return 0;
   }
 }
 module.exports = app
