d8cde7545f63c7dd825a1e228ddcdecb5941ddea
Jesusbaby <olayemifolakemi@yahoo.com>
2016-12-01 00:21:56 +0100

    add binary search solution

--- binarySearch/binarySearch.js
+++ binarySearch/binarySearch.js
@@ -1,1 +1,51 @@
+Array.prototype.toTwenty = function() {
+  var arr = [];
+  for (i = 1; i <= 20; i++) {
+    arr.push(i);
+  }
+  return arr;
+}
 
+Array.prototype.toForty = function() {
+  var arr = [];
+  for (var i = 2; i <= 40; i += 2) {
+    arr.push(i);
+  }
+  return arr;
+}
+
+Array.prototype.toOneThousand = function() {
+  var arr = [];
+  for (var i = 10; i <= 1000; i += 10) {
+    arr.push(i);
+  }
+
+  return arr;
+}
+
+function binarySearch(numbers, key) {
+  var max = numbers.length - 1;
+  var min = 0;
+  while (min <= max) {
+    mid = Math.floor((min + max) / 2);
+    if (numbers[mid] === key) {
+      return mid;
+    }
+
+    if (numbers[mid] < key) {
+      min = mid + 1;
+    } else if (numbers[mid] > key) {
+      max = mid - 1;
+    }
+  }
+
+  return -1;
+}
+
+Array.prototype.toTwenty = function() {
+  var arr = []
+  for (i = 1; i <= 20; i++) {
+    arr.push(i);
+  }
+  return arr;
+}
